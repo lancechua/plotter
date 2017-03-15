9e4f94311ba9c3da5b934d06ae30d37b5d1cd162
Lance Chua <lancerobinchua[at]gmail[dot]com>
2017-03-15 22:39:17 +0800

    added new addfn method that adds one function at a time but parses keyword arguments

--- plotter.py
+++ plotter.py
@@ -4,8 +4,7 @@ import collections
 import math
 
 class plotter:
-	def __init__(self, x_min, x_max, num=None):
-		if num is None: num = int((x_max - x_min) * 2) 
+	def __init__(self, x_min, x_max, num=200):
 		self.domain = np.linspace(x_min, x_max, num)
 
 	def add_linfns(self, m_list, b_list):
@@ -14,6 +13,14 @@ class plotter:
 			raise Exception('Please enter arguments as lists or tuples')
 
 		[plt.plot(self.domain, (lambda x: m * x + b)(self.domain)) for m, b in zip(m_list,b_list)]
+
+	def add_fn(self, f, f_kwargs={} ):
+		"""add a function, with optional keyword arguments in dict format.
+		"""
+		if not isinstance(f_kwargs, dict):
+			raise Exception('f_kwargs must be a dictionary.')	
+		f_param = lambda x: f(x, **f_kwargs)
+		plt.plot(self.domain, [f_param(a) for a in self.domain])	
 
 	def add_fns(self, f_list):
 		"""add any function; note order is not important now
@@ -30,7 +37,16 @@ if __name__ == '__main__':
 	def myfunc(x):
 		return 100000/(1+math.exp(-x))
 
+	def myf2(x,a):
+		return x ** a
+
 	graph = plotter(1,500)
 	graph.add_linfns((10,20,30,40),[0,0,0,0])
 	graph.add_fns([lambda x: x ** 1.5, lambda x: x ** 2, myfunc])
+	graph.add_fn(myfunc)
+	
+	kwargs = {'a' : -0.5}
+	graph.add_fn(myf2, kwargs)
+
+
 	graph.show()
